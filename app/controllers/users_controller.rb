4d4613dcc7ae6f266352da9ee8208de2d350d3e1
ShirinHasan13 <shirin10@chicagobooth.edu>
2024-07-24 19:55:12 +0000

    d

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
     render({ :template => "users_templates/all_users"})
   end
-  
+
   def show
     username = params.fetch("username")
     matching_users = User.where({ :username => username })
@@ -14,20 +14,20 @@ class UsersController < ApplicationController
 
     render({ :template => "user_templates/user_details"})
   end
-  
+
   def create
     user = User.new
     user.username = params.fetch("query_username")
     user.save
-    
+
     redirect_to("/users/#{user.username}")
   end
-  
+
   def update
     user_id = params.fetch("user_id")
     matching_users = User.where({ :id => user_id })
     the_user = matching_users.at(0)
-    
+
     the_user.username = params.fetch("query_username")
     the_user.save
     redirect_to("/users/#{user.username}")
